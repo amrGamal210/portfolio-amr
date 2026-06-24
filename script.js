1e6d2325c76261c651697a2a127261d47fa37d95
Amr Gamal Awad <122135155+amrGamal210@users.noreply.github.com>
2026-06-24 19:04:58 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+function toggleTheme(){
+document.body.classList.toggle("light");
+}
+
+// particles
+const canvas=document.getElementById("particles");
+const ctx=canvas.getContext("2d");
+
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+
+let particles=[];
+
+for(let i=0;i<80;i++){
+particles.push({
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+r:Math.random()*3,
+dx:(Math.random()-0.5),
+dy:(Math.random()-0.5)
+});
+}
+
+function animate(){
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+particles.forEach(p=>{
+p.x+=p.dx;
+p.y+=p.dy;
+
+if(p.x<0||p.x>canvas.width)p.dx*=-1;
+if(p.y<0||p.y>canvas.height)p.dy*=-1;
+
+ctx.beginPath();
+ctx.arc(p.x,p.y,p.r,0,Math.PI*2);
+ctx.fillStyle="#ff9800";
+ctx.fill();
+});
+
+requestAnimationFrame(animate);
+}
+
+animate();
+
+// PWA
+if('serviceWorker' in navigator){
+navigator.serviceWorker.register('sw.js');
+}
